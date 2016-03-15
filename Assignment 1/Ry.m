a0e1d3fb70981b321ad7b2275872978299f00159
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2016-03-15 17:13:54 -0300

    Added a way to load .obj files and basic transformations

---
+++ Assignment 1/Ry.m
@@ -0,0 +1,5 @@
+function [R] = Ry (theta)
+  R = [cos(theta), 	0.0, 	sin(theta), 	0.0;
+       0.0, 		1.0, 	0.0, 			0.0;
+       -sin(theta), 0.0, 	cos(theta),  	0.0; 
+       0.0, 		0.0, 	0.0,	 		1.0];
