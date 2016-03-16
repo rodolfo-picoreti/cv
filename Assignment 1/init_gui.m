fce05255e041ad13f8a95ea6004b5f422d4d58ce
felippe <mendonca.felippe@gmail.com>
2016-03-16 09:03:05 -0300

    bug setValue fixed

--- Assignment 1/init_gui.m
+++ Assignment 1/init_gui.m
@@ -9,13 +9,20 @@ handles.objectF = F;
 camera_path = 'reflex_camera.obj';
 [V, F] = load_obj(camera_path);
 V= [V';ones(1,length(V))];
-handles.cameraV = T(50, 50, 50)*S(15)*V;
+handles.cameraV = T(30, 40, 50)*S(15)*V;
 handles.cameraF = F;
 
-handles.cameraP = T(50, 50, 50)*[0; 0; 0; 1];
-set(handles.editObjTX,'Value',handles.cameraP(1));
-set(handles.editObjTY,'Value',handles.cameraP(2));
-set(handles.editObjTZ,'Value',handles.cameraP(3));
+handles.cameraP = T(30, 40, 50)*[0; 0; 0; 1];
+handles.w_axis = [-100, 100, -100, 100, -20, 130];
+
+
+set(handles.editObjTX,'String',num2str(handles.cameraP(1)));
+set(handles.editObjTY,'String',num2str(handles.cameraP(2)));
+set(handles.editObjTZ,'String',num2str(handles.cameraP(3)));
+
+set(handles.sliderObjTX,'Value',handles.cameraP(1)/(handles.w_axis(2)-handles.w_axis(1)));
+set(handles.sliderObjTY,'Value',handles.cameraP(2)/(handles.w_axis(4)-handles.w_axis(3)));
+set(handles.sliderObjTZ,'Value',handles.cameraP(3)/(handles.w_axis(6)-handles.w_axis(5)));
 
 axes(handles.worldFigure);
 trisurf(handles.objectF, ...
@@ -28,7 +35,6 @@ trisurf(handles.cameraF, ...
         handles.cameraV(1,:), handles.cameraV(2,:), handles.cameraV(3,:), ...
         'FaceColor', 'b', 'EdgeColor', 'none');
 
-handles.w_axis = [-100, 100, -100, 100, -20, 130];
 axis equal;
 axis(handles.w_axis);
 
