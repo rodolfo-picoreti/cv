b915d375ab0544bc0c6e99cf7ccf21e65c1b0c99
felippe <mendonca.felippe@gmail.com>
2016-03-27 16:07:50 -0300

    teste interface gráfica

--- Assignment 1/gui.m
+++ Assignment 1/gui.m
@@ -95,286 +95,202 @@ function varargout = gui_OutputFcn(hObject, eventdata, handles)
 % Get default command line output from handles structure
 varargout{1} = handles.output;
 
-
 % --- Executes on button press in resetCameraPosition.
 function resetCameraPosition_Callback(hObject, eventdata, handles)
   handles = resetCamera(handles);
-
 
 % --- Executes on button press in resetObjectPosition.
 function resetObjectPosition_Callback(hObject, eventdata, handles)
   handles = resetObject(handles);
 
+%% --- EDIT OBJ FUNCTIONS
+function editObjTX_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'ObjTX');
+function editObjTY_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'ObjTY');
+function editObjTZ_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'ObjTZ');
 
+function editObjRX_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'ObjRX');
 function editObjRY_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'ObjRY');
+function editObjRZ_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'ObjRZ');
+%% --- EDIT CAM FUNCTIONS
+function editCamTX_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'CamTX');
+function editCamTY_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'CamTY');
+function editCamTZ_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'CamTZ');
+  
+function editCamRX_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'CamRX');
+function editCamRY_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'CamRY');
+function editCamRZ_Callback(hObject, eventdata, handles)
+  handles = updateEdit(hObject, handles, 'CamRZ');
+%% --- SLIDER OBJ FUNCTIONS
+function sliderObjTX_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'ObjTX');
+function sliderObjTY_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'ObjTY');
+function sliderObjTZ_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'ObjTZ');
 
+function sliderObjRX_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'ObjRX');
+function sliderObjRY_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'ObjRY');
+function sliderObjRZ_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'ObjRZ');  
+%% --- SLIDER CAM FUNCTIONS
+function sliderCamTX_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'CamTX');
+function sliderCamTY_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'CamTY');
+function sliderCamTZ_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'CamTZ');
 
-% --- Executes during object creation, after setting all properties.
+function sliderCamRX_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'CamRX');
+function sliderCamRY_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'CamRY');
+function sliderCamRZ_Callback(hObject, eventdata, handles)
+  updateSlider(handles, 'CamRZ');
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  %% --- Executes during object creation, after setting all properties.
 function editObjRY_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
   end
 
-
-% --- Executes on slider movement.
-function sliderObjRX_Callback(hObject, eventdata, handles)
-  updateSlider(handles, ObjRX);
-  
-
-% --- Executes during object creation, after setting all properties.
 function sliderObjRX_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
 
-
-function editObjRX_Callback(hObject, eventdata, handles)
-
-
-% --- Executes during object creation, after setting all properties.
 function editObjRX_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
   end
 
-
-% --- Executes on slider movement.
-function sliderObjRZ_Callback(hObject, eventdata, handles)
-  updateSlider(handles, ObjRZ);
-  
-
-% --- Executes during object creation, after setting all properties.
 function sliderObjRZ_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
 
-
-function editObjRZ_Callback(hObject, eventdata, handles)
-  updateEdit(handles, 'ObjRZ');
-  from_range = [-180, 18
-
-
-% --- Executes during object creation, after setting all properties.
 function editObjRZ_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
   end
 
-
-% --- Executes on slider movement.
-function sliderObjRY_Callback(hObject, eventdata, handles)
-  updateSlider(handles, ObjRY);
-  
-
-% --- Executes during object creation, after setting all properties.
 function sliderObjRY_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
 
-
-% --- Executes on slider movement.
-function sliderObjTY_Callback(hObject, eventdata, handles)
-  updateSlider(handles, ObjTY);
-  
-
-% --- Executes during object creation, after setting all properties.
 function sliderObjTY_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
 
-
-function editObjTZ_Callback(hObject, eventdata, handles)
-
-
-% --- Executes during object creation, after setting all properties.
-function editObjTZ_CreateFcn(hObject, eventdata, handles)
-  if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-  end
-
-
-% --- Executes on slider movement.
-function sliderObjTZ_Callback(hObject, eventdata, handles)
-  updateSlider(handles, ObjTZ);
-  
-
-% --- Executes during object creation, after setting all properties.
 function sliderObjTZ_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
-
-
-function editObjTX_Callback(hObject, eventdata, handles)
-  updateEdit(handles, 'ObjTX');
-
-
-% --- Executes during object creation, after setting all properties.
+  
+function editObjTZ_CreateFcn(hObject, eventdata, handles)
+  if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+  end
+  
 function editObjTX_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
   end
-
-
-% --- Executes on slider movement.
-function sliderObjTX_Callback(hObject, eventdata, handles)
-  updateSlider(handles, ObjTX);
-  updateSlider(handles, 'ObjTX');
-  %m2str(value);
-
-
-% --- Executes during object creation, after setting all properties.
+  
 function sliderObjTX_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
-
-
-function editObjTY_Callback(hObject, eventdata, handles)
-
-
-% --- Executes during object creation, after setting all properties.
+  
 function editObjTY_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
       set(hObject,'BackgroundColor','white');
   end
 
-
-function editCamTY_Callback(hObject, eventdata, handles)
-
-
-% --- Executes during object creation, after setting all properties.
 function editCamTY_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
   end
 
-
-% --- Executes on slider movement.
-function sliderCamTX_Callback(hObject, eventdata, handles)
-  updateSlider(handles, CamTX);
-  
-
-% --- Executes during object creation, after setting all properties.
 function sliderCamTX_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
 
-
-function editCamTX_Callback(hObject, eventdata, handles)
-  updateEdit(handles, 'CamTX');
-
-
-% --- Executes during object creation, after setting all properties.
 function editCamTX_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
   end
 
-
-% --- Executes on slider movement.
-function sliderCamTZ_Callback(hObject, eventdata, handles)
-  updateSlider(handles, CamTZ);
-  
-
-% --- Executes during object creation, after setting all properties.
-function sliderCamTZ_CreateFcn(hObject, eventdata, handles)
-  if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor',[.9 .9 .9]);
-  end
-
-
-function editCamTZ_Callback(hObject, eventdata, handles)
-
-
-% --- Executes during object creation, after setting all properties.
 function editCamTZ_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
   end
 
+function sliderCamTZ_CreateFcn(hObject, eventdata, handles)
+  if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+  end
 
-% --- Executes on slider movement.
-function sliderCamTY_Callback(hObject, eventdata, handles)
-  updateSlider(handles, CamTY);
-  
-
-% --- Executes during object creation, after setting all properties.
 function sliderCamTY_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
 
-
-% --- Executes on slider movement.
-function sliderCamRY_Callback(hObject, eventdata, handles)
-  updateSlider(handles, CamRY);
-  
-
-% --- Executes during object creation, after setting all properties.
 function sliderCamRY_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
 
-
-function editCamRZ_Callback(hObject, eventdata, handles)
-
-
-% --- Executes during object creation, after setting all properties.
-function editCamRZ_CreateFcn(hObject, eventdata, handles)
-  if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-  end
-
-
-% --- Executes on slider movement.
-function sliderCamRZ_Callback(hObject, eventdata, handles)
-  updateSlider(handles, CamRZ);
-  
-
-% --- Executes during object creation, after setting all properties.
-function sliderCamRZ_CreateFcn(hObject, eventdata, handles)
-  if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor',[.9 .9 .9]);
-  end
-
-
-function editCamRX_Callback(hObject, eventdata, handles)
-
-
-% --- Executes during object creation, after setting all properties.
 function editCamRX_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white'); 
   end
 
+function editCamRZ_CreateFcn(hObject, eventdata, handles)
+  if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+  end
 
-% --- Executes on slider movement.
-function sliderCamRX_Callback(hObject, eventdata, handles)
-  updateSlider(handles, CamRX);
-  
+function sliderCamRZ_CreateFcn(hObject, eventdata, handles)
+  if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+  end
 
-% --- Executes during object creation, after setting all properties.
 function sliderCamRX_CreateFcn(hObject, eventdata, handles)
   if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
   end
 
-
-function editCamRY_Callback(hObject, eventdata, handles)
-
-
-% --- Executes during object creation, after setting all properties.
 function editCamRY_CreateFcn(hObject, eventdata, handles)
   if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
   end
-
 
 function slider20_CreateFcn(hObject, eventdata, handles)
 function slider19_CreateFcn(hObject, eventdata, handles)
